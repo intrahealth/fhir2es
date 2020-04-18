e2553fc6266cdeec7afe0dcb7df3293991e6ec21
Ally Shaban <allyshaban5@gmail.com>
2020-04-18 22:12:39 +0300

    initial upload

---
+++ exampleUsage.js
@@ -0,0 +1,14 @@
+const env = process.env.NODE_ENV || 'development';
+var config = require(`/home/ally/emNutt/server/config/config_${env}.json`);
+const { CacheFhirToES } = require('fhir2es')
+
+let caching = new CacheFhirToES({
+  ESBaseURL: config.elastic.baseURL,
+  ESUsername: config.elastic.username,
+  ESPassword: config.elastic.password,
+  ESMaxCompilationRate: config.elastic.max_compilations_rate,
+  FHIRBaseURL: config.macm.baseURL,
+  FHIRUsername: config.macm.username,
+  FHIRPassword: config.macm.password
+})
+caching.cache()
