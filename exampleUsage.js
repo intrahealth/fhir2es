a826995048e7f53ba1081fdd1a050dd1bcc729f6
Ally Shaban <allyshaban5@gmail.com>
2023-08-31 12:11:43 +0300

    performance enhancement with initialFilter

--- exampleUsage.js
+++ exampleUsage.js
@@ -5,12 +5,12 @@ let caching = new CacheFhirToES({
   ESPassword: '',
   ESMaxCompilationRate: '100000/1m',
   ESMaxScrollContext: '100000',
-  FHIRBaseURL: 'http://localhost:8080/hapi/fhir',
+  FHIRBaseURL: 'http://localhost:8081/bwihris/fhir',
   FHIRUsername: '',
   FHIRPassword: '',
   since: '', //use this to specify last updated time of resources to be processed
-  relationshipsIDs: [], //if not specified then all relationships will be processed
-  reset: false //will pull all resources if set to true
+  relationshipsIDs: ['ihris-es-report-pp-staffs'], //if not specified then all relationships will be processed
+  reset: true //will pull all resources if set to true
 })
 caching.cache().then(() => {
   console.log('Done')
